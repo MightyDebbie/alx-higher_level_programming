0827589e319c0e5fdcffcb80972a498cb8ab5305
MightyDebbie <MightyDebbie>
2023-03-09 12:42:58 -0800

    modulesupdate

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Program that prints the addition of all arguments"""
+    """Print the number of and list of arguments in a given program"""
     import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
