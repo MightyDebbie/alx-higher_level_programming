a0c6c2ed27f240c4711abd75c99c54c90e4a2782
MightyDebbie <MightyDebbie>
2023-04-16 10:34:10 -0700

    inputandoutputtask

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Includes the function "wrtie_file""""
+"""Module that writes into a file"""
 
 
 def write_file(filename="", text=""):
-    """returns the number of chars written to "filename" from "text" """
-    with open(filename, 'w', encoding='utf=8') as f:
-        return f.write(text)
+    """Function that writes into file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
+
